6e2340d2def66bce42c7fa4c1c8a31115d58a479
Aviral Jethalia <aviral.jethalia@mykaarma.com>
2020-10-30 04:53:22 +0530

    MYKAARMA-8284: changed hardcoded redirection logic for mkplay

--- pybossa/view/home.py
+++ pybossa/view/home.py
@@ -51,9 +51,9 @@ def home():
         data['historical_contributions'] = historical_projects
         rank_and_score = cached_users.rank_and_score(user_id)
         current_user.rank = rank_and_score['rank']
-    response = dict(template='/home/index.html', **data)
+    #response = dict(template='/home/index.html', **data)
     #return handle_content_type(response)
-    return redirect('/project/category/mkplaygames', code=302)
+    return redirect(url_for('projects.project_cat_index', category='mkplaygames'), code=302)
 
 
 @blueprint.route("about")
